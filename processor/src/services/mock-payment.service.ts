26bc604d1b385843fd36598d7e2318e7ca90ab4c
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-10 10:28:37 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -316,8 +316,8 @@ console.log('status-handler');
 	    payment_type: 'IDEAL',
 	    amount: '123',
 	    currency: 'EUR',
-  	    return_url: 'https://individual-payment.vercel.app/success',
-            error_return_url: 'https://individual-payment.vercel.app/failure',
+	    return_url: 'https://service-gxj31ubdem0d5a3yfzvyd735.europe-west1.gcp.sandbox.commercetools.app/success',
+	    error_return_url: 'https://service-gxj31ubdem0d5a3yfzvyd735.europe-west1.gcp.sandbox.commercetools.app/failure',
 	  },
 	  custom: {
 	    input1: 'currencyCode',
