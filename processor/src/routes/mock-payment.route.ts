feefca31e07d7c18857288d0d520479b14ed8461
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-09-15 15:12:38 +0530

    Update mock-payment.route.ts

--- processor/src/routes/mock-payment.route.ts
+++ processor/src/routes/mock-payment.route.ts
@@ -114,7 +114,7 @@ console.log('handle-novalnetResponse');
     fastify.get('/failure', async (request, reply) => {
     return reply.send('Payment was successful.');
   });
-    fastify.get('/v13', async (request, reply) => {
+    fastify.post('/v13', async (request, reply) => {
     return reply.send('Payment was successful.');
   });
   fastify.get('/success', async (request, reply) => {
