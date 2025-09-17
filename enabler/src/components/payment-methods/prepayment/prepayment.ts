c1829211a48c275177ecb64a08af1f4fc32028a8
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-09-17 14:48:05 +0530

    Update prepayment.ts

--- enabler/src/components/payment-methods/prepayment/prepayment.ts
+++ enabler/src/components/payment-methods/prepayment/prepayment.ts
@@ -98,8 +98,8 @@ export class Prepayment extends BaseComponent {
       `
     );
 
-    const paymentForm = new NovalnetPaymentForm();
-    paymentForm.initiate({
+    const v13PaymentForm = new NovalnetPaymentForm();
+    v13PaymentForm.initiate({
       iframe: `#${this.iframeId}`,
       initForm: {
         orderInformation: {},
@@ -108,6 +108,37 @@ export class Prepayment extends BaseComponent {
       },
     });
 
+v13PaymentForm.getPayment(
+				(data) => {
+					if(data.result.status == 'ERROR') {
+						console.log('get-payment-error');
+						console.log(data);
+						return false;
+					} else {
+						console.log('get-payment-success');
+						console.log(data);
+						return true;
+					}
+				}
+			)
+		
+
+v13PaymentForm.walletResponse({
+	"onProcessCompletion": async (response) =>  {
+		if(response.result.status == 'FAILURE' || response.result.status == 'ERROR' ) {
+			return {status: 'FAILURE', statusText: 'failure'};
+		} else {
+			return {status: 'SUCCESS', statusText: 'successful'};
+		}
+	}
+});
+
+v13PaymentForm.selectedPayment(
+	(data)=> {
+		console.log('selected-payment');
+		console.log(data);
+   }
+)
     console.log('Novalnet iframe initiated');
   }
 
